b23543013e89143329107536f43ea96a84ebbad3
vikttorcostta <victorfotografia12@gmail.com>
2023-12-21 17:24:13 -0300

    commit

--- CAP_01_REVISÃO_PHP/prog_11.php
+++ CAP_01_REVISÃO_PHP/prog_11.php
@@ -8,13 +8,24 @@
 <body>
 
     <?php
-        $soma = 0;
+
+        $soma = 1;
         $valor1 = 10;
         $valor2 = 20;
         $valor3 = 30;
 
-        $soma += $valor1;
-        $soma = 33;
+        $soma += $valor1; //soma fica com 100 == 10
+        $soma += $valor2; //soma fica com 30 == 10+20
+        $soma *= $valor3; //soma fica com 900 == 30*30
+        $soma %= 100; //soma fica com 0 == 900%100
+
+        echo $soma;
+
+        if ($soma == true and $soma == false){
+            print("verdadeiro");
+        }else{
+            print("false");
+        }
     ?>
-</body>
+</body> 
 </html>
