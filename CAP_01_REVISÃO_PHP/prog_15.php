1960a3b305eb310565feace3bffcd23045cad4ae
vikttorcostta <victorfotografia12@gmail.com>
2023-12-22 01:02:46 -0300

    commit

---
+++ CAP_01_REVISÃO_PHP/prog_15.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programa 15</title>
+</head>
+<body>
+    <?php
+        
+        for($i = 0; $i <= 100; $i++){
+            echo "A variavel \$i vale $i";
+            echo "<br>";
+        }
+    ?>
+    
+</body>
+</html>
---
+++ CAP_01_REVISÃO_PHP/prog_16.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programa 16</title>
+</head>
+<body>
+    <?php
+
+        $vetor = [1,2,3,4,5,6,7,8,9,10];
+        foreach($vetor as $v){
+            print"O valor atual é $v <br> ";
+        }
+
+        $vetor = ["um" => 1, "dois" => 2, "tres" => 3, "quatro" =>4, "cinco" =>5];
+        foreach($vetor as $chave => $valor){
+            print"O valor atual é [$chave] => $valor <br> ";
+          
+        }
+       
+       
+    ?>
+    
+</body>
+</html>
