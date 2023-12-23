84072df11d093f4506c4500c7c4cd1819d9c65b6
vikttorcostta <victorfotografia12@gmail.com>
2023-12-23 17:38:25 -0300

    commit

--- CAP_02_MELHORANDO_FORMULARIOS/valida_email.php
+++ CAP_02_MELHORANDO_FORMULARIOS/valida_email.php
@@ -3,29 +3,29 @@
 /***
  * PAULO VICTOR COSTA DE MELO
  * 23/12/2023
+ * 
  */
 
+
+
 //FUNÇÃO DE VALIDAÇÃO DE EMAIL
+function email_clear_validate(string $email_in){
+    $clear_email = filter_var($email_in, FILTER_SANITIZE_EMAIL);
+    $validate_email = filter_var($clear_email, FILTER_VALIDATE_EMAIL);
 
-function limpa_email(string $email_entrada){
-
-   $caracteres_invalidos = [" ", "/", ",", ".@", "@.", ";"]; 
-   //listagem de caracateres possiveis a erros de digitação
-
-   $caracteres_validos = ["", "", ".", "@", "@", "."]; 
-   //fazendo a substituição pelos caracteres corretos
-    
-   $email_saida = str_replace($caracteres_invalidos, $caracteres_validos, $email_entrada);
-   //substituição dos caracteres errados
-
-   $email_saida = filter_var($email_saida, FILTER_SANITIZE_EMAIL);
-   //limpeza geral de email
-
-   return filter_var($email_saida, FILTER_VALIDATE_EMAIL);
-   //retorna se o email é válido ou não
-
+    return $validate_email !== false ? $validate_email : false;
 }
 
 
+//FUNÇÃO DE VALIDAÇÃO DE USUARIO
+function user_validade_clear(string $user_validate)
+{
+    $clear_user = filter_var($user_validate, FILTER_SANITIZE_SPECIAL_CHARS);
+    if(strlen($clear_user) < 8 || strlen($clear_user) > 10){
+        return false;
+    }else{
+        return $clear_user;
+    }
+}
 
 
