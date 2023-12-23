b035090cfd05f21fe0d3db519aaa34abfa6edc99
vikttorcostta <victorfotografia12@gmail.com>
2023-12-23 16:59:55 -0300

    commit

--- CAP_02_MELHORANDO_FORMULARIOS/valida_email.php
+++ CAP_02_MELHORANDO_FORMULARIOS/valida_email.php
@@ -2,21 +2,27 @@
 
 /***
  * PAULO VICTOR COSTA DE MELO
- * 
+ * 23/12/2023
  */
 
 //FUNÇÃO DE VALIDAÇÃO DE EMAIL
 
 function limpa_email(string $email_entrada){
 
-   $caracteres_invalidos = [" ", "/", ",", ".@", "@.", ";"]; //listagem de caracateres passiveis a erros de digitação
-   $caracteres_validos = ["", "", ".", "@", "@", "."]; //fazendo a substituição pelos caracteres corretos
+   $caracteres_invalidos = [" ", "/", ",", ".@", "@.", ";"]; 
+   //listagem de caracateres possiveis a erros de digitação
+
+   $caracteres_validos = ["", "", ".", "@", "@", "."]; 
+   //fazendo a substituição pelos caracteres corretos
     
-   $email_saida = str_replace($caracteres_invalidos, $caracteres_validos, $email_entrada);//substituição dos caracteres errados
+   $email_saida = str_replace($caracteres_invalidos, $caracteres_validos, $email_entrada);
+   //substituição dos caracteres errados
 
-   $email_saida = filter_var($email_saida, FILTER_SANITIZE_EMAIL);//limpeza geral de email
+   $email_saida = filter_var($email_saida, FILTER_SANITIZE_EMAIL);
+   //limpeza geral de email
 
-   return filter_var($email_saida, FILTER_VALIDATE_EMAIL);// retorna se o email é válido ou não
+   return filter_var($email_saida, FILTER_VALIDATE_EMAIL);
+   //retorna se o email é válido ou não
 
 }
 
